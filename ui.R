8eab77d35b0907f95ff51d2dbe35405bf50c6a86
MarcoLeti <letico.marco@gmail.com>
2018-02-05 18:21:37 +0100

    insert description in ui

--- ui.R
+++ ui.R
@@ -33,7 +33,14 @@ shinyUI(fluidPage(
     
     mainPanel(
        plotOutput("distPlot"),
-       headerPanel(withMathJax("$$\\text{GBM Model: } S_0 \\exp\\left(\\left(\\mu - \\frac{\\sigma^2}{2}\\right)t + \\sigma W_t\\right) $$"))
+       headerPanel(withMathJax("$$\\text{GBM Model: } S_0 \\exp\\left(\\left(\\mu - \\frac{\\sigma^2}{2}\\right)t + \\sigma W_t\\right) $$")),
+       h4("To run the simulation you have to enter the following inputs on the side bar:"),
+       h4("Initial Stock Price is the current price of the stock;"),
+       h4("Drift rate is the expected rate of return;"),
+       h4("Yearly Standard Deviation is the volatility of the stock price;"),
+       h4("Number of Simulation represents how many simulation of stock price you want to display;"),
+       h4("In the side bar is also possible, through a check box, to set the seed to a fix value. Please mark the check box and select the value from the numeric box. If it is unmarked the seed will be assigned randomly.
+            As the calculation time increases with the number of simulation, there is a 'Submit' button to click as soon as the parameters are decided.")
     )
   )
 ))
